f74e18756fc94012846978f153167e783489a66b
Taylor Bell <taylorbell@gmail.com>
2014-03-02 14:00:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fortfort::Application.config.secret_key_base = 'bf5d8d21fef049d1cef4ad8d24630efac2f8ab79e43c3a3cd9415a3f113dba70a19fc95d5cc0c1c8a19491b2084ae05191ab3aee909b3653be8f45ebacd05581'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fortfort::Application.config.session_store :cookie_store, key: '_fortfort_session'
