33e7126ddd0bc79a2e986ae941d96cc409c30fa6
Taylor Bell <taylorbell@gmail.com>
2014-03-18 17:26:03 -0600

    fix TV route, add link color to frontpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Fortfort::Application.routes.draw do
+   root 'schedule#index'
+
   get "sched2/index"
-  get "tv/index"
+  #get "tv/index"
   get "schedule/index"
   get "home/index"
+  get 'tv' => 'tv#index'
+ 
+ resource :tvs, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'schedule#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
